68071210ccc3ec0117992df64fbde4c23549e5a9
thanhduy29 <nguyenthanhduy0429@gmail.com>
2023-07-30 00:44:51 +0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
 
+import './header.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Layout/Header";
+import React from "react";
+import Head from "next/head";
+
+const Homepage = () => {
+  return (
+    <>
+      <Head>
+        <title>HOME SPORT STORE</title>
+      </Head>
+      <Header />
+    </>
+  );
+};
+
+export default Homepage;
